d14aaac0642a62af9ccead986fc2e1147bc90887
Bruno Neves <brunu.neves@gmail.com>
2017-04-24 17:27:25 -0300

    v-for vue

--- vue2/src/main.js
+++ vue2/src/main.js
@@ -2,8 +2,14 @@ import Vue from 'vue'
 import {Time} from './time'
 
 new Vue({
-  el: '#app',
-  data: {
-    time: new Time('Palmeiras', require('./assets/palmeiras_60x60.png'))
-  }
+    el: '#app',
+    data: {
+        times: [
+            new Time('Palmeiras', require('./assets/palmeiras_60x60.png')),
+            new Time('Santos', require('./assets/santos_60x60.png')),
+            new Time('Flamengo', require('./assets/flamengo_60x60.png')),
+        ],
+
+
+    }
 })
