b3510bc188278ecf205694edcf1a2e7f6505fdd3
Bruno Neves <brunu.neves@gmail.com>
2017-05-23 10:33:11 -0300

    iniciar jogo

--- vue2/src/main.js
+++ vue2/src/main.js
@@ -4,25 +4,30 @@ import {Time} from './time';
 require('style-loader!css-loader!bootstrap/dist/css/bootstrap.min.css');
 require('bootstrap');
 
-new Vue({
+let myVue = new Vue({
     el: '#app',
     data: {
         times: [
             new Time('Palmeiras', require('./assets/palmeiras_60x60.png')),
             new Time('Santos', require('./assets/santos_60x60.png')),
             new Time('Flamengo', require('./assets/flamengo_60x60.png')),
+
             new Time('Cruzeiro', require('./assets/cruzeiro_60x60.png')),
             new Time('Fluminense', require('./assets/fluminense_60x60.png')),
             new Time('São Paulo', require('./assets/sao_paulo_60x60.png')),
+
             new Time('Sport', require('./assets/sport_60x60.png')),
             new Time('Internacional', require('./assets/internacional_60x60.png')),
             new Time('Grêmio', require('./assets/gremio_60x60.png')),
+
             new Time('Figueirense', require('./assets/figueirense_60x60.png')),
             new Time('Botafogo', require('./assets/botafogo_60x60.png')),
             new Time('Corinthians', require('./assets/corinthians_60x60.png')),
+
             new Time('Vitória', require('./assets/vitoria_60x60.png')),
             new Time('Atletico PR', require('./assets/atletico-pr_60x60.png')),
             new Time('Atletico MG', require('./assets/atletico_mg_60x60.png')),
+
             new Time('Coritiba', require('./assets/coritiba_60x60.png')),
         ],
 
@@ -35,8 +40,19 @@ new Vue({
                 time:null,
                 gols:0
             }
-        }
-
-
+        },
     },
-})
+    created(){
+        let indexCasa = Math.floor(Math.random() * 16),
+        indexFora = Math.floor(Math.random() * 16);
+
+        this.novoJogo.casa.time = this.times[indexCasa];
+        this.novoJogo.casa.gols = 0;
+        this.novoJogo.fora.time = this.times[indexFora];
+        this.novoJogo.fora.gols = 0;
+
+    }
+
+
+});
+console.log(myVue);
